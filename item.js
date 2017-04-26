7a0317a9b354272eb9023566c58543263a041679
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-04-26 15:57:56 +0200

    Item

---
+++ item.js
@@ -0,0 +1,36 @@
+var Item = Backbone.model.extend({
+	_validateFeature: function(name, value) {
+		return typeof name === "string" && typeof value === "string";
+	},
+
+	setFeature: function(name, value) {
+		var features;
+
+		if(!this._validateFeature(name, value)) {
+			return false;
+		}
+
+		if(this.has("features")) {
+			features = this.get("features");
+		} else {
+			features = [];
+		}
+		features[name] = value;
+
+		this.set("features", features);
+
+		return true;
+	},
+
+	/**
+	 * @see http://stackoverflow.com/questions/5096549/how-to-override-backbone-sync
+	 *
+	 * @param method
+	 * @param model
+	 * @param options
+	 */
+	"sync": function(method, model, options) {
+		// TODO: implement
+		alert('SYNC! ' + method + " " + model + " " + options);
+	}
+});
