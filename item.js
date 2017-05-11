face085223ebc8476127fc16b3c829ea7c3cd1af
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-05-11 23:22:19 +0200

    Esperimenti di alternative a Backbone

--- item.js
+++ item.js
@@ -1,38 +1,49 @@
-var Item = Backbone.Model.extend({
-	idAttribute: "code",
+function Item() {
+	this.features = {};
+	this.defaultFeatures = {};
+	this.code = null;
+}
 
-	_validateFeature: function(name, value) {
-		return typeof name === "string" && typeof value === "string";
-	},
-
-	setFeature: function(name, value) {
-		var features;
-
-		if(!this._validateFeature(name, value)) {
-			return false;
-		}
-
-		if(this.has("features")) {
-			features = this.get("features");
-		} else {
-			features = [];
-		}
-		features[name] = value;
-
-		this.set("features", features);
-
+// TODO: use a proxy to build another object with null features when removed, new features when added, etc... for "update" queries
+Item.prototype.setFeature = function(name, value) {
+	if(this.isValidFeatureName(name)) {
+		this.features[name] = value;
 		return true;
-	},
-
-	/**
-	 * @see http://stackoverflow.com/questions/5096549/how-to-override-backbone-sync
-	 *
-	 * @param method
-	 * @param model
-	 * @param options
-	 */
-	"sync": function(method, model, options) {
-		// TODO: implement
-		alert('SYNC! ' + method + " " + model + " " + options);
+	} else {
+		return false;
 	}
-});
+};
+
+Item.prototype.setCode = function(code) {
+	if(this.isValidCode(code)) {
+		this.code = code;
+		return true;
+	} else {
+		return false;
+	}
+};
+
+Item.prototype.setDefaultFeature = function(name, value) {
+	if(this.isValidFeatureName(name)) {
+		this.defaultFeatures[name] = value;
+		return true;
+	} else {
+		return false;
+	}
+};
+
+Item.prototype.removeFeature = function(name) {
+	delete this.features[name];
+};
+
+Item.prototype.removeFeature = function(name) {
+	delete this.defaultFeatures[name];
+};
+
+Item.prototype.isValidFeatureName = function(name) {
+	return typeof name === 'string';
+};
+
+Item.prototype.isValidCode = function(code) {
+	return this.isValidFeatureName(code);
+};
