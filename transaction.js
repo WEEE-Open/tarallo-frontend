b2466574ac7a5c4df65814d28bcc23dd9c6a51d0
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-05-20 18:01:03 +0200

    Classe Transaction riscritta

--- transaction.js
+++ transaction.js
@@ -1,49 +1,33 @@
-var Transaction = Backbone.Model.extend({
-	setNotes: function(notes) {
-		this.set("notes", notes);
-	},
+class Transaction extends FrameworkObject {
+	constructor(trigger) {
+		super(trigger);
+		this.reset();
+	}
 
 	// TODO: passare già come tree è più facile, ma ha senso? Passarli singoli rende complicato ricostruire l'albero (il server non lo fa e rischia di dare errore se sono in disordine...)
 	// TODO: il server al momento non genera codici. Farglieli generare.
-	addNew: function(item) {
-		var newItems;
-
-		if(this.has("newItems")) {
-			newItems = this.get("newItems");
-
-		} else {
-			newItems = [];
-		}
-		newItems.push(item);
-
-		this.set("newItems", newItems);
-
-		return true;
-	},
-
-	addUpdated: function(item) {
-		var updatedItems;
-
-		if(this.has("updatedItems")) {
-			updatedItems = this.get("updatedItems");
-
-		} else {
-			updatedItems = [];
-		}
-		updatedItems.push(item);
-
-		this.set("updatedItems", updatedItems);
-
-		return true;
-	},
+	/**
+	 * Add new Item
+	 *
+	 * @param {Item} item
+	 */
+	add(item) {
+		this.create.push(item);
+	}
 
 	/**
-	 * @param method
-	 * @param model
-	 * @param options
+	 * Add an item changeset(?) to upload
+	 *
+	 * @todo use an ItemUpdate class
+	 * @param {Item} item
 	 */
-	"sync": function(method, model, options) {
-		// TODO: implement
-		alert('SYNC! ' + method + " " + model + " " + options);
+	addUpdated(item) {
+		this.update.push(item);
 	}
-});
+
+	reset() {
+		this.create = [];
+		this.update = [];
+		this.notes = null;
+	}
+}
