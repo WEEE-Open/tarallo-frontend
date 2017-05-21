f9e552580d5434d01f59cd06275d3fa33aa4bbbd
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-05-21 23:24:00 +0200

    Documentazione + individuati bug assurdi

--- translations.js
+++ translations.js
@@ -5,17 +5,34 @@ class Translations extends FrameworkObject {
 	 */
 	constructor(trigger, code) {
 		super(trigger);
-		this.setLanguage(code)
+		this._setLanguage(code)
 	}
 
+	/**
+	 * Set language code and trigger a "change" event.
+	 *
+	 * @param {String} code
+	 * @private
+	 */
 	setLanguage(code) {
+		this._setLanguage(code);
+		this.trigger('change');
+	}
+
+	/**
+	 * Set language code without triggering an event (used in constructor only)
+	 *
+	 * @param {String} code
+	 * @private
+	 */
+	_setLanguage(code) {
 		if(code === 'it-IT') {
+			// TODO: this ends up in a "this" that isn't it's "this".
 			this.features = Translations.it.features;
 		} else {
 			// TODO: really handle multiple languages
 			new Error('Unknown language ' + code);
 		}
-		this.trigger('change');
 	}
 
 	//noinspection JSMethodCanBeStatic
