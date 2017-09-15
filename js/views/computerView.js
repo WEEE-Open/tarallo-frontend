795005c1b9e2e219c716151cf8c1ee21ea018d7a
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-09-15 18:51:50 +0200

    ComputerView, parte 1

--- js/views/computerView.js
+++ js/views/computerView.js
@@ -13,10 +13,20 @@ class ComputerView extends Framework.View {
 	 */
 	constructor(element, item, language, logs) {
 		super(element);
+		if (this.item.features.get("type") !== "computer") {
+			throw new TypeError('Cannot create ComputerView for item type "' + this.item.features.get("type") + '", only "computer" is allowed');
+		}
+
 		this.item = item;
 		this.language = language;
 		this.logs = logs;
 
 		this.el.appendChild(document.getElementById("template-computer").content.cloneNode(true));
+		// TODO: handle nulls
+		this.el.querySelector(".brand").textContent = this.item.features.get("brand");
+		this.el.querySelector(".model").textContent = this.item.features.get("model");
+		this.el.querySelector(".header button").addEventListener("click", this.editClick.bind(this));
 	}
+
+
 }
