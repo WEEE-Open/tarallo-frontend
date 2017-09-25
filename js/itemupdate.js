4ae2e4fef3a3d92ba865d7725e19f0da46f97d28
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-09-25 22:47:40 +0200

    ItemUpdate che aggiorna feature

--- js/itemupdate.js
+++ js/itemupdate.js
@@ -2,24 +2,47 @@ class ItemUpdate extends Item {
 	constructor(item) {
 		super();
 		this.originalItem = item;
+
+		this.featuresDiff = new Map();
+		this.featuresMerged = new Map();
+
+		// TODO: is this needed?
+		for(let [k,v] of this.originalItem.features) {
+			this.featuresMerged.set(k,v);
+		}
+	}
+
+	get features() {
+		return this.featuresMerged;
 	}
 
 	/**
 	 * @see Item.setFeature
 	 */
 	setFeature(name, value) {
-		if(super.setFeature(name, value)) {
-			if(value === null) {
-
-			}
+		if(this.originalItem.features.get(name) === value) {
+			this.featuresDiff.delete(value);
+		} else {
+			this.featuresDiff.set(name, value);
 		}
+
+		if(value === null) {
+			this.featuresMerged.delete(name);
+		} else {
+			this.featuresMerged.set(name, value);
+		}
+	}
+
+	get featuresCount() {
+		// TODO: this - features set to null + new features
+		//return this.features.size;
 	}
 
 	/**
 	 * @see Item.setDefaultFeature
 	 */
 	setDefaultFeature(name, value) {
-		// TODO: implement
+		throw new Error("Cannot set default features in item changesets (how did you even get here!?)");
 	}
 
 	/**
